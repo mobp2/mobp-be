1d1a537ebd630bfe64b1d49c825534afed32729e
unifolio0 <boho82ha@gmail.com>
2025-05-17 19:57:47 +0900

    chore: 환경 세팅

---
+++ src/test/kotlin/com/example/mobp2/Mobp2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mobp2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Mobp2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
